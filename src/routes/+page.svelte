003682dc57e027c1ede5983fd5252c85c73d6696
Naphat Amundsen <naphat@live.no>
2023-08-06 00:23:30 +0200

    New homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Heading, P } from 'flowbite-svelte';
+</script>
+
+<div class="flex h-screen">
+  <div class="m-auto">
+    <Heading class="mb-4">Welcome to my page</Heading>
+    <P>I am Naphat Amundsen, a software developer</P>
+  </div>
+</div>
